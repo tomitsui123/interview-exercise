13c0de9da0b39359cca33225e97e45def7dd1627
tommy <tomitsui123@gmail.com>
2023-02-19 02:56:56 +0800

    add create todo list api

--- routes/todo.ts
+++ routes/todo.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { Request, Router, Response, NextFunction } from 'express'
 import createError from 'http-errors'
-var router = express.Router()
+
+var router: Router = express.Router()
 const todos = [{ id: 1, user: 'tommy', name: 'Complete the coding test' }]
 router.get('/:user', function (req, res, next) {
   const filteredTodo = todos.filter((todo) => todo.user === req.params.user)
@@ -8,5 +9,17 @@ router.get('/:user', function (req, res, next) {
     return next(createError(404, 'Not Found'))
   }
   return res.json(filteredTodo)
+})
+
+router.post('/:user', (req: Request, res: Response) => {
+  const { name } = req.body
+  const { user } = req.params
+  const newTodo = {
+    id: todos.length + 1,
+    name,
+    user
+  }
+  todos.push(newTodo)
+  res.status(201).json(newTodo)
 })
 export default router
