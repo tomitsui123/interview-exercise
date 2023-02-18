f66afe6dcaccf7a45c0a4b1edf5238c5d1bee88b
tommy <tomitsui123@gmail.com>
2023-02-19 02:56:56 +0800

    update test script for GET /todos/:user

--- app.test.ts
+++ app.test.ts
@@ -1,6 +1,22 @@
-import app from "./app"
+import request from 'supertest'
+import app from './app'
 describe('TODO api', () => {
-    it('GET /todos/{userName} --> return array of todos', () => {})
+  it('GET /todos/{userName} --> return array of todos', () => {
+    return request(app)
+      .get('/todos/tommy')
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+              id: expect.any(Number),
+              user: expect.any(String),
+            }),
+          ])
+        )
+      })
+  })
 
-    it('POST /todos/{userName} --> create a todo item of a specific user', () => {})
+  it('POST /todos/{userName} --> create a todo item of a specific user', () => {})
 })
