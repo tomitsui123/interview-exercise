5ada265b9e603f322b2c7495d717b7abdbcb4a1a
tommy <tomitsui123@gmail.com>
2023-02-19 02:56:56 +0800

    prepare test script

---
+++ app.test.ts
@@ -0,0 +1,6 @@
+import app from "./app"
+describe('TODO api', () => {
+    it('GET /todos/{userName} --> return array of todos', () => {})
+
+    it('POST /todos/{userName} --> create a todo item of a specific user', () => {})
+})
