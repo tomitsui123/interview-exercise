9970634104d0773ff182f7b17c4a061b3219666f
tommy <tomitsui123@gmail.com>
2023-02-19 02:56:56 +0800

    remove unused code in app.ts

--- app.ts
+++ app.ts
@@ -2,8 +2,7 @@ import express, { Express } from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
+import indexRouter from './routes/index'
 
 var app: Express = express()
 
@@ -14,6 +13,5 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 export default app
