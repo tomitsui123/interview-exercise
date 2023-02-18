9464640ccea6758595a049c9cd4e5b489c9078af
tommy <tomitsui123@gmail.com>
2023-02-19 02:56:56 +0800

    update error handling for GET todo list api

--- routes/todo.ts
+++ routes/todo.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import createError from 'http-errors'
 var router = express.Router()
 const todos = [{ id: 1, user: 'tommy', name: 'Complete the coding test' }]
-router.get('/:user', function (req, res) {
+router.get('/:user', function (req, res, next) {
   const filteredTodo = todos.filter((todo) => todo.user === req.params.user)
-  res.json(filteredTodo)
+  if (filteredTodo.length === 0) {
+    return next(createError(404, 'Not Found'))
+  }
+  return res.json(filteredTodo)
 })
 export default router
